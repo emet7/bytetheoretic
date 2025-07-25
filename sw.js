ba2990b958526513ca32ac46f44ae3deed109fd6
emet7 <emet@bytetheoretic.com>
2025-07-25 10:33:00 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-hello?
+const CACHE_NAME = 'dsx-pwa-cache';
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/bit-character.png'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
